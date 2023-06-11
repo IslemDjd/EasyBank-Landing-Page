d86fd5850058300f37c1668d50ae60613045a1ee
IslemDjd <islemas23@gmail.com>
2023-06-11 03:11:09 +0100

    Final

--- main.js
+++ main.js
@@ -9,12 +9,34 @@ function aUnhover(element){
 
 }
 
+
+function svgHover(element){
+    element.style.fill = "hsl(136, 65%, 51%)";
+}    
+
+function svgUnhover(element){
+    element.style.fill = "#FFF";
+
+}
+
 const toggleButton = document.getElementsByClassName('toggle')[0];
 const nav_btn = document.getElementsByClassName('nav-btn')[0];
 const inter = document.getElementsByClassName('interface')[0];
+var closePNG = document.getElementById("close");
 
 toggleButton.addEventListener('click',() =>{
     nav_btn.classList.toggle('active');
     inter.classList.toggle('activeInterface');
+    closePNG.style.display = "block";
+    toggleButton.style.display = "none";
 
 });
+
+closePNG.addEventListener('click',() =>{
+    nav_btn.classList.toggle('active');
+    inter.classList.toggle('activeInterface');
+    closePNG.style.display = "none";
+    toggleButton.style.display = "block";
+
+});
+
